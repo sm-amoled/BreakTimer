c67a25367dceca6cf5f89f1e80b93b4cac5233dc
sm-amoled <moktak072@naver.com>
2022-11-06 18:46:22 +0900

    🎉 Initial Project Setting

---
+++ BreakTimer/BreakTimer Watch App/BreakTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakTimerApp.swift
+//  BreakTimer Watch App
+//
+//  Created by Park Sungmin on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct BreakTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
